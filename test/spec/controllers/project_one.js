8c305fc3a1df81fc19679688e65fa2b109585f00
Paul Kennedy <c10319445@mydit.ie>
2015-03-28 12:28:47 +0000

    Updated Tests.

--- test/spec/controllers/project_one.js
+++ test/spec/controllers/project_one.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: ProjectOneCtrl', function () {
 
   // load the controller's module
   beforeEach(module('experimentalApp'));
@@ -11,12 +11,8 @@ describe('Controller: AboutCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    AboutCtrl = $controller('ProjectOneCtrl', {
       $scope: scope
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
