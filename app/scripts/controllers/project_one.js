ba7423bdead72a565e7833fc20e6cac9061c608e
Paul Kennedy <c10319445@mydit.ie>
2015-03-28 11:35:20 +0000

    Updated the Age Validation Logic. Added Cross over function to detect overlapping segments

--- app/scripts/controllers/project_one.js
+++ app/scripts/controllers/project_one.js
@@ -10,14 +10,70 @@
  */
 angular.module('experimentalApp')
   .controller('ProjectOneCtrl', function ($scope) {
-  	$scope.age = null;
+  	$scope.age = '';
 
-  	// Function to Calculate a persons age based on the current date.
-  	$scope.calculateAge = function calculateAge() { 
-	  	var age = new Date($scope.age);
-	    age = Date.now() - age.getTime();
-	    age = new Date(age);
-	    return Math.abs(age.getUTCFullYear() - 1970);
+  	// Tuples which define the age range for an Infant, Child and Adult
+  	$scope.infant = [2,0];
+  	$scope.child = [12,2];
+  	$scope.adult = [150,12];
+  	$scope.childHotel = [18,0];
+
+  	// Function to Calculate the difference in age between the Current Date and the input Date if the date is in the past.
+  	$scope.calculateAge = function calculateAge() {
+	  	var ageInput = new Date($scope.age);
+	  	var date;
+
+	  	// Only calculate age difference if the Age Input is less than the Current Date
+	  	if(ageInput.getTime() < Date.now()){
+		    var difference = Date.now() - ageInput.getTime();
+		    date = new Date(difference);
+		    date = Math.abs(date.getUTCFullYear() - 1970);
+		}
+
+		// If NaN set to null
+	    if(isNaN(date)){
+	    	date = null;
+	    }
+	    return date;
+	};
+
+	// Function which determines if an item is within an Age Range Specified
+	// Usage: ageRange(18,2);
+	$scope.ageRange = function ageRange(from,to){
+		var currentAge = $scope.calculateAge();
+		if(currentAge !== null){
+			if(currentAge < from && currentAge >= to){
+				return(true);
+			}
+		}
+	};
+
+	$scope.ageCrossOver = function ageCrossOver(flightFrom,flightTo,hotelFrom,hotelTo,limitTo,limitFrom){
+		var currentAge = $scope.calculateAge();
+		var flightValid = false;
+		var hotelValid = false;
+
+		if(currentAge !== null && currentAge >= limitFrom && currentAge < limitTo){
+			console.log('From:'+limitFrom);
+			console.log('To:'+limitTo);
+			console.log('Current:'+currentAge);
+			if(currentAge < flightFrom && currentAge >= flightTo){
+				flightValid = true;
+			}
+
+			if(currentAge < hotelFrom && currentAge >= hotelTo){
+				hotelValid = true;
+			}
+			console.log('Hotel Valid:'+hotelValid);
+			console.log('Flight Valid:'+flightValid);
+		}
+		if(hotelValid !== flightValid){
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	};
   });
 
